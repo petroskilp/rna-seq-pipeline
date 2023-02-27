78e4a91c152d510c9ae616fadd620cf464d0b139
Luiz Pedro Petroski <petroskilp@gmail.com>
2023-02-27 18:15:28 -0300

    update workflow add heatmap

--- workflow/scripts/plot-heatmap.R
+++ workflow/scripts/plot-heatmap.R
@@ -14,6 +14,6 @@ df <- as.data.frame(colData(dds)[,snakemake@params[["heatmap_labels"]]])
 ntd <- normTransform(dds)
 rownames(df) <- colnames(assay(ntd)[select,])
 svg(snakemake@output[[1]])
-pheatmap(assay(ntd)[select,], cluster_rows=FALSE, show_rownames=FALSE,
-         cluster_cols=FALSE, annotation_col=df)
+pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=FALSE,
+         cluster_cols=TRUE, annotation_col=df)
 dev.off()
