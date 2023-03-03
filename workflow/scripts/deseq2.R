9164b60b5dbf408a2c6997c61176023064f61cc2
Luiz Pedro Petroski <petroskilp@gmail.com>
2023-03-03 18:59:09 -0300

    update workflow add heatmap

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -15,7 +15,21 @@ if (snakemake@threads > 1) {
 dds <- readRDS(snakemake@input[[1]])
 
 contrast <- c("condition", snakemake@params[["contrast"]])
+norm_counts = counts(dds, normalized=T)
 res <- results(dds, contrast=contrast, parallel=parallel)
+
+coldata <- read.table(snakemake@params[["samples"]], header=TRUE, row.names="sample_name", check.names=FALSE)
+coldata <- coldata[order(row.names(coldata)), , drop=F]
+condition <- snakemake@params[["model"]]
+condition <- sub('~', '', condition)
+for (i in unique(coldata$as.formula(condition)))
+{
+    mean <- data.frame(mean=rowMeans(norm_counts[,c(metadata[coldata$as.formula(condition)==i,]$sample_name)]))
+    mean <- cbind(rownames(mean), mean)
+    rownames(mean) <- NULL
+    colnames(mean)<-c('row', paste("baseMean_", i, sep=""))
+    res<-merge(res, mean)
+}
 # shrink fold changes for lowly expressed genes
 # use ashr so we can use `contrast` as conversion to coef is not trivial
 # see https://bioconductor.org/packages/release/bioc/vignettes/DESeq2/inst/doc/DESeq2.html#extended-section-on-shrinkage-estimators
